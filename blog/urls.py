18b5854021461b6efc00376fbef62446c2d485af
Sudipto Shahin <sudipto.shine@gmail.com>
2022-03-24 22:24:34 +0600

    pagination done!

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ from blog.views import (
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
+    UserPostListView,
 )
 
 urlpatterns = [
@@ -25,6 +26,8 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
 
     path('about/', views.about, name='blog-about'),
 ]
