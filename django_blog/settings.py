33cc167eef7dd9e1d3afcf1bf8acb763a79e9b5a
Sudipto Shahin <sudipto.shine@gmail.com>
2022-03-26 19:27:30 +0600

    password reset done

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # where to save
@@ -141,3 +142,17 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 # login url
 LOGIN_URL = 'login'
+
+
+# User password reset
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sudipto.shine@gmail.com'
+EMAIL_HOST_PASSWORD = 'lgfvjahjmzppqemg'
+
+
+# for production
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
